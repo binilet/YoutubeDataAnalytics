1e55c20078c147734efae869b0f4080b6b37201c
binilet <let_me_know47@yahoo.com>
2024-05-11 17:10:17 +0300

    added langchain database connection

--- redash/handlers/chat.py
+++ redash/handlers/chat.py
@@ -4,16 +4,31 @@ from redash.handlers.base import (
 )
 import os
 from openai import OpenAI
+
 VARIABLE_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(
   api_key=VARIABLE_KEY
 )
+
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_openai import ChatOpenAI
+
+# PostgreSQL connection details
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+
+DB_URI = f"postgresql+psycopg2://postgres:{POSTGRES_USER}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
 
 class ChatResource(BaseResource):
     def post(self):
         try:
             value = request.get_json()
             question = value.get('question')
+
             completion = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
@@ -23,7 +38,31 @@ class ChatResource(BaseResource):
             )
             answer = completion.choices[0].message.content
             response_data = {"answer": answer}
+
+            youtube_chat = ChatWithYoutubeDatabase()
+            _db = youtube_chat.get_db_connection(DB_URI)
+
+
+
+
             return jsonify(response_data), 200
+        
         except Exception as error:
             print(error)
             return jsonify({"error": "An error occurred"}), 500
+        
+#create a connection with postgres db with langchain
+
+class ChatWithYoutubeDatabase():
+    def get_db_connection(db_uri):
+        try:
+            db = SQLDatabase.from_uri(db_uri)
+            print(db.dialect)
+            print(db.get_usable_table_names())
+            return db
+        except Exception as e:
+            print(e)
+    
+    
+    
+    
